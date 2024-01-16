80e2b93b516484c8792b5f6e118b8ef5e4817cb0
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2024-01-16 14:39:06 +0900

    [Update]

--- ManagedDxlGame/program/game/hikanyan_laboratory/userInterface/InputManager.cpp
+++ ManagedDxlGame/program/game/hikanyan_laboratory/userInterface/InputManager.cpp
@@ -1,73 +1,9 @@
 #include "InputManager.h"
 
-InputManager::InputManager() {
-    // コンストラクタでの初期化
-}
-
-InputManager::~InputManager() {
-    // リソースの解放
-}
-
-void InputManager::initialize() {
-    // 入力デバイスの初期化
-}
-
-void InputManager::update() {
-    // 入力の状態を更新
-}
-
-bool InputManager::is_key_down(tnl::Input::eKeys key) {
-    // キーボードの特定のキーが押されているかをチェック
-    return tnl::Input::IsKeyDown(key);
-}
-
-bool InputManager::is_key_up(int key)
+void InputManager::update(float delta_time)
 {
-    return false;
+    if (tnl::Input::IsKeyDown(eKeys::KB_SPACE))
+    {
+        Debug::log("KB_SPACE");
+    }
 }
-
-bool InputManager::is_key_pressed(int key)
-{
-    return false;
-}
-
-bool InputManager::is_mouse_down(int button)
-{
-    return false;
-}
-
-bool InputManager::is_mouse_up(int button)
-{
-    return false;
-}
-
-bool InputManager::is_mouse_pressed(int button)
-{
-    return false;
-}
-
-tnl::Vector3 InputManager::get_mouse_position()
-{
-    return {};
-}
-
-bool InputManager::is_pad_button_down(int button)
-{
-    return false;
-}
-
-bool InputManager::is_pad_button_up(int button)
-{
-    return false;
-}
-
-bool InputManager::is_pad_button_pressed(int button)
-{
-    return false;
-}
-
-tnl::Vector3 InputManager::get_pad_stick_position(int stick)
-{
-    return {};
-}
-
--- ManagedDxlGame/program/game/hikanyan_laboratory/userInterface/InputManager.h
+++ ManagedDxlGame/program/game/hikanyan_laboratory/userInterface/InputManager.h
@@ -3,32 +3,9 @@
 
 class InputManager : public MonoBehaviour {
 public:
-    // コンストラクタとデストラクタ
-    InputManager();
-    ~InputManager();
-
-    // 入力の初期化と更新メソッド
-    void initialize();
-    void update();
-
-    // キーボード入力の処理メソッド
-    bool is_key_down(tnl::Input::eKeys key);
-    bool is_key_up(int key);
-    bool is_key_pressed(int key);
-
-    // マウス入力の処理メソッド
-    bool is_mouse_down(int button);
-    bool is_mouse_up(int button);
-    bool is_mouse_pressed(int button);
-    tnl::Vector3 get_mouse_position();
-
-    // ゲームパッド入力の処理メソッド
-    bool is_pad_button_down(int button);
-    bool is_pad_button_up(int button);
-    bool is_pad_button_pressed(int button);
-    tnl::Vector3 get_pad_stick_position(int stick);
-
-private:
-    // 入力状態を保持するためのプライベートメンバ変数
-    // 例: キーボード状態、マウス状態、ゲームパッド状態など
+    InputManager() = default;
+    virtual ~InputManager() override = default;
+    
+    void update(float delta_time) override;
+    void fixed_update(float delta_time) override;
 };
