837cfbebd2f36f56513f034683d7aa8252290ae0
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2024-01-16 16:10:04 +0900

    リポジトリを最初から作らなくても、ライブラリを更新できる方法を見つけたためこれ以上このリポジトリでは更新しない。 バグがあったときのためリポジトリは削除はしない。 ShotingGameDxl2023へ作業を引き継ぐ

---
+++ ManagedDxlGame/program/game/hikanyan_laboratory/entity/player/Player.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "tnl_vector3.h"
+
+class Player {
+public:
+    tnl::Vector3 position;
+    Health health;
+
+    // 他のプレイヤー関連の属性とメソッド
+};
---
+++ ManagedDxlGame/program/game/hikanyan_laboratory/entity/player/PlayerMove.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "IPlayerController.h"
+
+class PlayerMove : public IPlayerController {
+public:
+    void Update(Player& player) override {
+        // 移動ロジックを実装
+    }
+};
+
